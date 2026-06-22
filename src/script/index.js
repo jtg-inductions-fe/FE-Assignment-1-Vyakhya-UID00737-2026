815dea846b9fbc4caf82f3ef4f20145bdfdc2870
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-06-22 10:28:27 +0530

    VN_A1_01: hamburger working on mobile & tablet screens is added

--- src/script/index.js
+++ src/script/index.js
@@ -5,11 +5,17 @@ const overlay = document.querySelector('.navbar__overlay');
 hamburger.addEventListener('click', () => {
     menuLinks.classList.toggle('navbar__menu--active');
     overlay.classList.toggle('navbar__overlay--active');
+<<<<<<< HEAD
     document.body.classList.toggle('body--menu-open');
+=======
+>>>>>>> 5387756 (VN_A1_01: hamburger working on mobile & tablet screens is added)
 });
 
 overlay.addEventListener('click', () => {
     menuLinks.classList.remove('navbar__menu--active');
     overlay.classList.remove('navbar__overlay--active');
+<<<<<<< HEAD
     document.body.classList.toggle('body--menu-open');
+=======
+>>>>>>> 5387756 (VN_A1_01: hamburger working on mobile & tablet screens is added)
 });
