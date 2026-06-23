71bdabfee6f10c754549332660d2099b4ac68c52
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-06-23 10:43:58 +0530

    VN_A1_01: blocked screen moving when hamburger is opened

--- src/script/index.js
+++ src/script/index.js
@@ -5,9 +5,11 @@ const overlay = document.querySelector('.navbar__overlay');
 hamburger.addEventListener('click', () => {
     menuLinks.classList.toggle('navbar__menu--active');
     overlay.classList.toggle('navbar__overlay--active');
+    document.body.classList.toggle('body--menu-open');
 });
 
 overlay.addEventListener('click', () => {
     menuLinks.classList.remove('navbar__menu--active');
     overlay.classList.remove('navbar__overlay--active');
+    document.body.classList.toggle('body--menu-open');
 });
