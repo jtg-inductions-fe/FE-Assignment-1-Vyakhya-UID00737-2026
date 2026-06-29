918341b331982bcf0f191a40dd1cb56e0cc30399
vyakhyanamdev <vyakhya.namdev@joshtechnologygroup.com>
2026-06-29 10:41:41 +0530

    VN_A1_03: rebased changes done from branch VN_A1_02

--- src/script/index.js
+++ src/script/index.js
@@ -2,14 +2,34 @@ const hamburger = document.querySelector('.navbar__hamburger');
 const menuLinks = document.querySelector('.navbar__menu');
 const overlay = document.querySelector('.navbar__overlay');
 
-hamburger.addEventListener('click', () => {
-    menuLinks.classList.toggle('navbar__menu--active');
-    overlay.classList.toggle('navbar__overlay--active');
-    document.body.classList.toggle('body--menu-open');
-});
+// logic for toggling in navigation menu
+const openMenu = () => {
+    menuLinks.classList.add('navbar__menu--active');
+    overlay.classList.add('navbar__overlay--active');
+    document.body.classList.add('body--menu-open');
+};
 
-overlay.addEventListener('click', () => {
+const closeMenu = () => {
     menuLinks.classList.remove('navbar__menu--active');
     overlay.classList.remove('navbar__overlay--active');
-    document.body.classList.toggle('body--menu-open');
+    document.body.classList.remove('body--menu-open');
+};
+
+const toggleMenu = () => {
+    if (menuLinks.classList.contains('navbar__menu--active')) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+};
+
+hamburger.addEventListener('click', toggleMenu);
+overlay.addEventListener('click', closeMenu);
+document.addEventListener('keydown', (e) => {
+    if (
+        e.key === 'Escape' &&
+        menuLinks.classList.contains('navbar__menu--active')
+    ) {
+        closeMenu();
+    }
 });
